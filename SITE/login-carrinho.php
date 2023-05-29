0c4eba666aad9af3fbaf2651f0930f383ec36f71
Micael-William <mw931991@gmail.com>
2023-05-29 12:06:38 -0300

    implementação do carrinho

--- SITE/login-carrinho.php
+++ SITE/login-carrinho.php
@@ -19,7 +19,7 @@
 </head>
 
 <body>
-    <header style=" width: 100%;" class="header">
+    <header style=" width: 100%; height: 15vh; display: flex; align-items: center; justify-content: space-between; padding: 20px;" class="header">
         <a href="index.php"><img src="../imagens/conteudo/LogoODCt.png" class="logo" alt="logo organicos de casa"></a>
         <nav>
             <ul class="lista-links">
@@ -28,19 +28,30 @@
                 <li><a style="color: #fff;" class="menu" href="../PGMS/cliente/login.php">Conta</a></li>
             </ul>
         </nav>
+        
     </header>
+
+  
 
     <!-- <section class="secao-logo-carrinho">
         <div class="container">
         </div>
     </section> -->
 
-    <h2 style="text-align: center; margin-top: 20px;">Carrinho</h2>
-
     <div class="banner-oferta">
+        <h2 style="text-align: center; margin-top: 20px;">Carrinho</h2>
         <p>A gente leva até você vegetais fresquinhos & sem frescura</p>
         <!-- <img class="logo-oferta" src="https://naturaldaterra.com.br/media/wysiwyg/HIF-0013-23I_-_FEIRA_-_CATEGORIA_-_970x150_-_NT_-_01.png?auto=webp&format=png&quality=85" alt=""> -->
     </div>
+
+      <header class="cabecalho-filtro">
+        <nav>
+            <a class="filtro-frutas" href="#"><img src="../IMAGENS/frutas-2.png" alt="">Frutas</a>
+            <a class="filtro-legumes" href="#"><img src="../IMAGENS/cenoura.png" alt="">Legumes</a>
+            <a class="filtro-verduras" href="#"><img src="../IMAGENS/vegetal (1).png" alt="">Verduras</a>
+            <a class="filtro-todos-os-produtos" href="#">Todos os produtos</a>
+        </nav>
+    </header>
 
     <div class="container-modal">
         <div class="legendas">
@@ -49,7 +60,7 @@
                 Compra realizada com sucesso
             </p>
         </div>
-        <button onclick="fecharModal()">fechar</button> 
+        <button onclick="fecharModal()">fechar</button>
     </div>
 
     <!-- lista aba carrinho -->
@@ -59,7 +70,8 @@
                 <table>
                     <thead>
                         <tr>
-                            <th>Produto</th>
+                       
+                            <th><img  src="../IMAGENS/carrinho.png" alt=""> Produto</th>
                             <th>Preço</th>
                             <th>Quantidade</th>
                             <th>Total</th>
@@ -67,7 +79,7 @@
                         </tr>
                     </thead>
                     <tbody>
-
+                     
                     </tbody>
                 </table>
             </section>
@@ -93,6 +105,14 @@
             </aside>
         </div>
     </main>
-    <script src="../js/listaProdutos.js"></script>
+
+    <!-- <section class="secao-logo-carrinho">
+        <div class="container">
+        </div>
+    </section> -->
+    <!-- <footer>
+        <h3>Copyright © Orgânicos de casa. Todos os direitos reservados.</h3>
+    </footer> -->
+    <script type="module" src="../js/listaProdutos.js"></script>
 </body>
 </html>
--- js/listaProdutos.js
+++ js/listaProdutos.js
@@ -1,86 +1,224 @@
-
-const listaLegumes = [
-    { produto: 'Moranga', categoria: 'Verdura', preco: 4.25, codproduto: 7, verduraMoranga: '../imagens/Produtos/Verdura/Moranga.png' },
-    { produto: 'Alface Crespa', categoria: 'Verdura', preco: 2.9, codproduto: 8, verduraAlface: '../imagens/Produtos/Verdura/Alface Crespa.png' },
-    { produto: 'Cebolinha', categoria: 'Verdura', preco: 3.5, codproduto: 9, verduraCebolinha: '../imagens/Produtos/Verdura/Cebolinha.png' },
-    { produto: 'Couve', categoria: 'Verdura', preco: 2.5, codproduto: 10, verduraCouve: '../imagens/Produtos/Verdura/Couve.png' },
-    { produto: 'Rabanete', categoria: 'Verdura', preco: 7.7, codproduto: 11, verduraRabenete: '../imagens/Produtos/Verdura/Rabenete.png' },
-    { produto: 'Salsa', categoria: 'Verdura', preco: 3.5, codproduto: 12, verduraSalsa: '../imagens/Produtos/Verdura/Salsa.png' },
-    { produto: 'Cebola Roxa', categoria: 'Verdura', preco: 3.7, codproduto: 13, legumeCebola: '../imagens/Produtos/Legume/Cebola Roxa.png' },
-    { produto: 'Cenoura', categoria: 'Verdura', preco: 1.5, codproduto: 14, legumeCenoura: '../imagens/Produtos/Legume/Cenoura.png' },
-    { produto: 'Mandioquinha', categoria: 'Verdura', preco: 14, codproduto: 15, legumeMandioquinha: '../imagens/Produtos/Legume/Mandioquinha.png' },
-    { produto: 'Milho Verde', categoria: 'Verdura', preco: 7, codproduto: 16, legumeMilhoVerde: '../imagens/Produtos/Legume/Milho Verde.png' },
-    { produto: 'Pimentão Verde', categoria: 'Verdura', preco: 3.9, codproduto: 17, legumePimentãoVerde: '../imagens/Produtos/Legume/Pimentão Verde.png' },
-    { produto: 'Tomate', categoria: 'Verdura', preco: '6.9', codproduto: 18, legumeTomate: '../imagens/Produtos/Legume/Tomate.png' }
-]
-
-const listaFrutas = [
-    { produto: 'Abacate', categoria: 'Fruta', preco: '6,00', codproduto: 1, fruta: '../imagens/Produtos/Fruta/Abacate.png', quantidade: 0 },
-    { produto: 'Abacaxi', categoria: 'Fruta', preco: '7,90', codproduto: 2, fruta: '../imagens/Produtos/Fruta/Abacaxi.png', quantidade: 0 },
-    { produto: 'Laranja', categoria: 'Fruta', preco: '0,90', codproduto: 3, fruta: '../imagens/Produtos/Fruta/Laranja.png', quantidade: 0 },
-    { produto: 'Limão', categoria: 'Fruta', preco: '0,50', codproduto: 4, fruta: '../imagens/Produtos/Fruta/Limão.png', quantidade: 0 },
-    { produto: 'Maça', categoria: 'Fruta', preco: '0,80', codproduto: 5, fruta: '../imagens/Produtos/Fruta/Maça.png', quantidade: 0 },
-    { produto: 'Morango', categoria: 'Fruta', preco: '13,00', codproduto: 6, fruta: '../imagens/Produtos/Fruta/Morango.png', quantidade: 0 }
+const listaProdutos = [
+    //Fruta
+    { produto: 'Abacate', categoria: 'Fruta', preco: '6,00', codproduto: 1, imagemProduto: '../imagens/Produtos/Fruta/Abacate.png', quantidade: 0 },
+    { produto: 'Abacaxi', categoria: 'Fruta', preco: '7,90', codproduto: 2, imagemProduto: '../imagens/Produtos/Fruta/Abacaxi.png', quantidade: 0 },
+    { produto: 'Laranja', categoria: 'Fruta', preco: '0,90', codproduto: 3, imagemProduto: '../imagens/Produtos/Fruta/Laranja.png', quantidade: 0 },
+    { produto: 'Limão', categoria: 'Fruta', preco: '0,50', codproduto: 4, imagemProduto: '../imagens/Produtos/Fruta/Limão.png', quantidade: 0 },
+    { produto: 'Maça', categoria: 'Fruta', preco: '0,80', codproduto: 5, imagemProduto: '../imagens/Produtos/Fruta/Maça.png', quantidade: 0 },
+    { produto: 'Morango', categoria: 'Fruta', preco: '13,00', codproduto: 6, imagemProduto: '../imagens/Produtos/Fruta/Morango.png', quantidade: 0 },
+    //Verdura
+    // { produto: 'Moranga', categoria: 'Verdura', preco: 4.25, codproduto: 7, imagemProduto: '../imagens/Produtos/Verdura/Moranga.png' },
+    // { produto: 'Alface Crespa', categoria: 'Verdura', preco: 2.9, codproduto: 8, imagemProduto: '../imagens/Produtos/Verdura/Alface Crespa.png' },
+    // { produto: 'Cebolinha', categoria: 'Verdura', preco: 3.5, codproduto: 9, imagemProduto: '../imagens/Produtos/Verdura/Cebolinha.png' },
+    // { produto: 'Couve', categoria: 'Verdura', preco: 2.5, codproduto: 10, imagemProduto: '../imagens/Produtos/Verdura/Couve.png' },
+    // { produto: 'Rabanete', categoria: 'Verdura', preco: 7.7, codproduto: 11, imagemProduto: '../imagens/Produtos/Verdura/Rabanete.png' },
+    // { produto: 'Salsa', categoria: 'Verdura', preco: 3.5, codproduto: 12, imagemProduto: '../imagens/Produtos/Verdura/Salsa.png' },
+    // Legume
+    // { produto: 'Cebola Roxa', categoria: 'Legume', preco: 3.7, codproduto: 13, imagemProduto: '../imagens/Produtos/Legume/Cebola Roxa.png' },
+    // { produto: 'Cenoura', categoria: 'Legume', preco: 1.5, codproduto: 14, imagemProduto: '../imagens/Produtos/Legume/Cenoura.png' },
+    // { produto: 'Mandioquinha', categoria: 'Legume', preco: 14, codproduto: 15, imagemProduto: '../imagens/Produtos/Legume/Mandioquinha.png' },
+    // { produto: 'Milho Verde', categoria: 'Legume', preco: 7, codproduto: 16, imagemProduto: '../imagens/Produtos/Legume/Milho Verde.png' },
+    // { produto: 'Pimentão Verde', categoria: 'Legume', preco: 3.9, codproduto: 17, imagemProduto: '../imagens/Produtos/Legume/Pimentão Verde.png' },
+    // { produto: 'Tomate', categoria: 'Legume', preco: '6.9', codproduto: 18, imagemProduto: '../imagens/Produtos/Legume/Tomate.png' }
 ]
 
 
 
 const secaoItens = document.querySelector('tbody')
-const btnRemover = document.querySelector('.botao-remover')
+import { mais, menos, criaModal, fecharModal } from "./modal.js";
+mais
+menos
+criaModal
+fecharModal
 
 function mostrarProdutos() {
     secaoItens.classList.add("secao-itens-titulo");
     secaoItens.innerHTML = "";
-    listaFrutas.forEach(item => {
+    listaProdutos.forEach((item, posicao) => {
+        const verifica = item.imagemProduto.length > 0 && item.produto.length > 0 && item.categoria.length > 0 && item.preco.length > 0 ?
+        
         secaoItens.innerHTML += `
-        <tr>
-            <td>
-                <div class="product">
-                    <img class="logo-produto" src="${item.fruta}" alt="" />
-                    <div style="text-align: center" class="info">
-                        <div class="name">${item.produto}</div>
-                        <div class="category">Categoria</div>
-                        <div class="category">${item.categoria}</div>
+            <tr>
+                <td>
+                    <div class="product">
+                        <img class="logo-produto" src="${item.imagemProduto}" alt="" />
+                        <div style="text-align: center" class="info">
+                            <div class="name">${item.produto}</div>
+                            <div class="category">Categoria</div>
+                            <div class="category">${item.categoria}</div>
+                        </div>
                     </div>
-                </div>
-            </td>
-            <td>R$ ${item.preco}</td>
-            <td>
-                <div class="qty">
-                    <button><i class="bx bx-minus"></i></button>
-                    <span>0</span>
-                    <button><i class="bx bx-plus"></i></button>
-                </div>
-            </td>
-            <td>R$ 240</td>
-            <td>
-                <button class="remove"><i class="bx bx-x"></i></button>
-            </td>
-       </tr>
+                </td>   
+                <td>R$ ${item.preco}</td>
+                <td>
+                    <div class="qty">
+                    <button ${posicao} onclick="menos()" class="a">-</button>
+                        <input  min=0 class="campo-valor" type="number">
+                    <button ${posicao} onclick="mais()" class="b">+</button>
+                    </div>
+                </td>
+                <td>R$ 0.0</td>
+                <td>
+                    <button class="remove"><i class="bx bx-x"></i></button>
+                </td>
+            </tr>
         `
+        : ''
+
+        return verifica 
+      
     })
 }
 
-
 mostrarProdutos()
 
-function criaModal(){
-    const body = document.querySelector("body")
-    const modal = document.querySelector(".container-modal")
-    body.style.opacity = '0.5'
-    body.style.animation = 'animate'
-    body.style.boxShadow = '1rem 0.5rem 1rem 0rem'
-    body.style.animationDuration = '1s'
-    modal.style.display = 'block'
-    body.style.background = '#fff'
+import listaDeProdutos from "./produtos.js";
+const botaoFrutas = document.querySelector(".filtro-frutas")
+const botaoLegumes = document.querySelector(".filtro-legumes")
+const botaoVerduras = document.querySelector(".filtro-verduras")
+const botaoTodosOsProdutos = document.querySelector(".filtro-todos-os-produtos")
 
-}  
 
-function fecharModal(){
-    const modal = document.querySelector(".container-modal")
-    const body = document.querySelector("body")
-    body.style.opacity = ''
-    body.style.background = ''
-    modal.style.display = 'none'
-}   
+botaoFrutas.addEventListener('click', produtoFrutas)
+botaoLegumes.addEventListener('click', produtoLegumes)
+botaoVerduras.addEventListener('click', produtoVerdura)
+botaoTodosOsProdutos.addEventListener('click', todosOsProdutos)
+
+function produtoFrutas() {
+    const secaoItens = document.querySelector('tbody')
+    secaoItens.innerHTML = ""
+
+    for (let i = 0; i < listaDeProdutos.fruta.length; i++) {
+        secaoItens.innerHTML += `
+            <tr>
+                <td>
+                    <div class="product">
+                        <img class="logo-produto" src="${listaDeProdutos.fruta[i].imagemProduto}" alt="${listaDeProdutos.fruta[i].produto}">
+                        <div style="text-align: center" class="info">
+                            <div class="name">${listaDeProdutos.fruta[i].produto}</div>
+                            <div class="category">Categoria</div>
+                            <div class="category">${listaDeProdutos.fruta[1].categoria}</div>
+                        </div>
+                    </div>
+                </td>   
+                <td>R$ ${listaDeProdutos.fruta[i].preco}</td>
+                <td>
+                    <div class="qty">
+                    <button ${i} onclick="menos()" class="a">-</button>
+                        <input  min=0 class="campo-valor" type="number">
+                    <button ${i} onclick="mais()" class="b">+</button>
+                    </div>
+                </td>
+                <td>R$ 0.0</td>
+                <td>
+                    <button class="remove"><i class="bx bx-x"></i></button>
+                </td>
+            </tr>
+        `
+    }
+}
+
+function produtoLegumes() {
+    const secaoItens = document.querySelector('tbody')
+    secaoItens.innerHTML = ""
+
+    for (let i = 0; i < listaDeProdutos.legume.length; i++) {
+        secaoItens.innerHTML += `
+            <tr>
+                <td>
+                    <div class="product">
+                        <img class="logo-produto" src="${listaDeProdutos.legume[i].imagemProduto}" alt="${listaDeProdutos.legume[i].produto}">
+                        <div style="text-align: center" class="info">
+                            <div class="name">${listaDeProdutos.legume[i].produto}</div>
+                            <div class="category">Categoria</div>
+                            <div class="category">${listaDeProdutos.legume[1].categoria}</div>
+                        </div>
+                    </div>
+                </td>   
+                <td>R$ ${listaDeProdutos.legume[i].preco}</td>
+                <td>
+                    <div class="qty">
+                    <button ${i} onclick="menos()" class="a">-</button>
+                        <input  min=0 class="campo-valor" type="number">
+                    <button ${i} onclick="mais()" class="b">+</button>
+                    </div>
+                </td>
+                <td>R$ 0.0</td>
+                <td>
+                    <button class="remove"><i class="bx bx-x"></i></button>
+                </td>
+            </tr>
+        `
+    }
+}
+
+function produtoVerdura() {
+    const secaoItens = document.querySelector('tbody')
+    secaoItens.innerHTML = ""
+
+    for (let i = 0; i < listaDeProdutos.verdura.length; i++) {
+        secaoItens.innerHTML += `
+            <tr>
+                <td>
+                    <div class="product">
+                        <img class="logo-produto" src="${listaDeProdutos.verdura[i].imagemProduto}" alt="${listaDeProdutos.verdura[i].produto}">
+                        <div style="text-align: center" class="info">
+                            <div class="name">${listaDeProdutos.verdura[i].produto}</div>
+                            <div class="category">Categoria</div>
+                            <div class="category">${listaDeProdutos.verdura[1].categoria}</div>
+                        </div>
+                    </div>
+                </td>   
+                <td>R$ ${listaDeProdutos.verdura[i].preco}</td>
+                <td>
+                    <div class="qty">
+                    <button ${i} onclick="menos()" class="a">-</button>
+                        <input  min=0 class="campo-valor" type="number">
+                    <button ${i} onclick="mais()" class="b">+</button>
+                    </div>
+                </td>
+                <td>R$ 0.0</td>
+                <td>
+                    <button class="remove"><i class="bx bx-x"></i></button>
+                </td>
+            </tr>
+        `
+    }
+}
+
+function todosOsProdutos() {
+    const secaoItens = document.querySelector('tbody')
+    secaoItens.innerHTML = ""
+    
+    for (let i = 0; i < listaDeProdutos.produto.length; i++) {
+        secaoItens.innerHTML += `
+            <tr>
+                <td>
+                    <div class="product">
+                        <img class="logo-produto" src="${listaDeProdutos.produto[i].imagemProduto}" alt="${listaDeProdutos.produto[i].produto}">
+                        <div style="text-align: center" class="info">
+                            <div class="name">${listaDeProdutos.produto[i].produto}</div>
+                            <div class="category">Categoria</div>
+                            <div class="category">${listaDeProdutos.produto[i].categoria}</div>
+                        </div>
+                    </div>
+                </td>   
+                <td>R$ ${listaDeProdutos.produto[i].preco}</td>
+                <td>
+                    <div class="qty">
+                    <button ${i} onclick="menos()" class="a">-</button>
+                        <input  min=0 class="campo-valor" type="number">
+                    <button ${i} onclick="mais()" class="b">+</button>
+                    </div>
+                </td>
+                <td>R$ 0.0</td>
+                <td>
+                    <button class="remove"><i class="bx bx-x"></i></button>
+                </td>
+            </tr>
+        `
+    }
+}
+
+
 
