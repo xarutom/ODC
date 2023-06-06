992ef0d9a9641355f5b64f8795e2065d4c5eafd4
Gustavo Carvalho <gutyribeiro2015@gmail.com>
2023-06-06 17:40:38 -0300

    ready aesthetics

--- SITE/sobre.php
+++ SITE/sobre.php
@@ -31,8 +31,10 @@
 <body>
     
 <section class="presentation">
-    <h1>Bem-vindo(a) à Orgânicos de Casa.</h1>
-        <p>
+    <section class="part">
+
+        <h1 class="welcome">Bem-vindo(a) à Orgânicos de Casa.</h1>
+        <p class="into" id="text">
             Somos uma equipe dedicada e apaixonados que se esforça para entregar e poder oferecer o melhor do que temos a oferecer. Fundada em Abril de 2022, por universitários da Fmu - Faculdade Metropolitanas Unidas, a ODC surge diante de debates em sala para montagem de um projeto inovador com intuíto de ajudar aqueles que não estão em condição de ingressar no mercado Orgânicos, facilitando a Agricultura familiar e deixando sua vida mais saudável.
         </p>
         <br>
@@ -40,33 +42,34 @@
         <h2 class="left">EQUIPE</h2>
         <div class="imgtext-left">
 
-            <p class="left">
+            <p class="left" id="text">
                 Nossa equipe é composta por jovens que trabalham desde a estrutura de nossas tecnologias até nossas decisões. Nosso principal desejo é conseguir chegar com você a um lugar nunca visto antes, todos podemos sonhar e crescer juntos. 
             </p>
             <img class="imgleft" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.gAlnOjS2Rcsc_Af3GgPGiAHaFj%26pid%3DApi&f=1&ipt=ade6d2b645b4c3285a18ed26e12edb62b2ad7faa256b5d4d05dbe590bb6b980c&ipo=images" alt="">
         </div>
-            
+        
             <h2 class="right">O QUE IRÁ ENCONTRAR AQUI?</h2>
             <div class="imgtext-right">
                 <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwebstockreview.net%2Fimages%2Favocado-clipart-happy-6.png&f=1&nofb=1&ipt=c402cf0a69502d650692a2064344432692bbf13aadd35cfef42e6bb26c533d75&ipo=images" alt="" class="imgright">
-                <p class="right">
+                <p class="right" id="text">
                     Neste aconchegante lugarzinho você terá inúmeras opções comidas orgânicas longe de alimentos transgênicos que podem atacar diretamente sua saúde de forma negativa, passamos por uma rígida avaliação de higiêne, processos e colheita de suas compras, assegurando a melhor qualidade para nossos consumidores.
                 </p>
             </div>
-
-                <h2 class="left">PARA AGRICULTORES</h2>
+            
+            <h2 class="left">PARA AGRICULTORES</h2>
                 <div class="imgtext-left">
-                    <p class="left">
+                    <p class="left" id="text">
                         Ei perai, você é um agricultor familiar? Foca aqui então que temos a coisa certa pra você.
                         Sabemos da dificuldade que muitos passam para se sustentar neste meio de mercado, portanto temos uma oferta para você! Se cadastrando e passando por todas nossas avaliações de qualidade você pode se tornar um parceiro e vender aqui em nosso site, fazendo seus anúncios e vendendo em nossa plataforma. Ficou interessado? Então faz o seguinte, clica nesse link aqui 
                         |<strong><a href="#">seja fornecedor</a></strong>|
                     </p>
                     <img class="imgleft" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.UabToyfzP3zu8AEVmCUVBAHaD7%26pid%3DApi&f=1&ipt=77191d4aee6b8d60fd49b15ba7344074a832f770f0c67abb7b4453dba66e5597&ipo=images" alt="">
                 </div>
-                    
+            </section>
+                
                 <p class="final">Agradecemos por visitar o nosso site e esperamos que você encontre o que procura aqui, caso tenha gostado e achado nossa ideia interssante compartilhe com outras pessoas para que possamos alcançar e ajudar o máximo de pessoas. Fique a vontade para navegar e explorar nosso conteúdo, caso tenha alguma dúvida estamos sempre abertos para perguntas, então entre em contato agora mesmo e não perca mais nenhuma promoção.</p>
-                        
-</section>
+                
+            </section>
         
     
 
